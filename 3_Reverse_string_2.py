3a102a170c65e1c3a042bce5f433700d471dcf8b
Madhusudan Gautam <madhusudangtm@gmail.com>
2022-10-16 16:14:13 +0530

    Add files via upload

---
+++ 3_Reverse_string_2.py
@@ -0,0 +1,11 @@
+# Reverse string approch 2
+
+m = input("Enter a word or line: ")
+x = ""
+for i in m:
+    x = i + x
+print(x)    
+
+#Output
+Enter a word or line: I am EdYoda Data Science student
+tneduts ecneicS ataD adoYdE ma I
